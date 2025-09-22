26e0eb98d72af26576af0d17829d4d55113ce9ea
fmway <fm18lv@gmail.com>
2025-09-22 19:35:19 +0700

    drop(ktechlab): fuck deprecated dependencies

--- pkgs/top-level/by-name.nix
+++ pkgs/top-level/by-name.nix
@@ -2,7 +2,6 @@
 with pkgs; {
   android-translation-layer = callPackage ../by-name/an/android-translation-layer/package.nix {};
   cage-xtmapper = callPackage ../by-name/ca/cage-xtmapper/package.nix {};
-  ktechlab = callPackage ../by-name/kt/ktechlab/package.nix {};
   qrc = callPackage ../by-name/qr/qrc/package.nix {};
   voiden = callPackage ../by-name/vo/voiden/package.nix {};
   xdman = callPackage ../by-name/xd/xdman/package.nix {};
